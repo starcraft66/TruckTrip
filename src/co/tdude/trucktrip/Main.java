05b0ce20782396b639bd8dcf170d5a278688c6df
Tristan Gosselin-Hane <starcraft66@gmail.com>
2017-11-03 11:08:43 -0400

    Added working crud

--- src/co/tdude/trucktrip/Main.java
+++ src/co/tdude/trucktrip/Main.java
@@ -10,6 +10,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -17,6 +18,7 @@ import javafx.stage.Stage;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main extends Application{
 
@@ -45,19 +47,72 @@ public class Main extends Application{
         table.setEditable(true);
 
         TableColumn truckNumberCol = new TableColumn("Truck Number");
+        truckNumberCol.setMinWidth(100);
+        truckNumberCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Integer>("truckNumber")
+        );
         TableColumn driverNumberCol = new TableColumn("Driver Number");
+        driverNumberCol.setMinWidth(100);
+        driverNumberCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Integer>("driverNumber")
+        );
         TableColumn coDriverNumberCol = new TableColumn("Co Driver Number");
+        coDriverNumberCol.setMinWidth(100);
+        coDriverNumberCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Integer>("coDriverNumber")
+        );
         TableColumn tripNumberCol = new TableColumn("Trip Number");
+        tripNumberCol.setMinWidth(100);
+        tripNumberCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Integer>("tripNumber")
+        );
         TableColumn departureTimeCol = new TableColumn("Departure time");
+        departureTimeCol.setMinWidth(100);
+        departureTimeCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Date>("departureTime")
+        );
         TableColumn returnTimeCol = new TableColumn("Return time");
+        returnTimeCol.setMinWidth(100);
+        returnTimeCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Date>("arrivalTime")
+        );
         TableColumn mileageCol = new TableColumn("Mileage");
+        mileageCol.setMinWidth(100);
+        mileageCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Double>("mileage")
+        );
         TableColumn gasUsedCol = new TableColumn("Gas Used");
+        gasUsedCol.setMinWidth(100);
+        gasUsedCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Double>("gasUsed")
+        );
         TableColumn gasPurchasedCol = new TableColumn("Gas Purchased");
+        gasPurchasedCol.setMinWidth(100);
+        gasPurchasedCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Double>("gasPurchased")
+        );
         TableColumn gasPriceCol = new TableColumn("Gas Price");
+        gasPriceCol.setMinWidth(100);
+        gasPriceCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, Double>("gasPrice")
+        );
         TableColumn stateCol = new TableColumn("State");
+        stateCol.setMinWidth(100);
+        stateCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, String>("state")
+        );
         TableColumn modelCol = new TableColumn("Model");
+        modelCol.setMinWidth(100);
+        modelCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, String>("model")
+        );
         TableColumn colorCol = new TableColumn("Color");
+        colorCol.setMinWidth(100);
+        colorCol.setCellValueFactory(
+                new PropertyValueFactory<TruckTrip, String>("color")
+        );
 
+        table.setItems(data);
         table.getColumns().addAll(truckNumberCol, driverNumberCol, coDriverNumberCol, tripNumberCol, departureTimeCol, returnTimeCol, mileageCol, gasUsedCol, gasPurchasedCol,gasPriceCol, stateCol, modelCol, colorCol);
 
         final TextField addTruckNumber = new TextField();
@@ -125,6 +180,16 @@ public class Main extends Application{
                 addTruckNumber.clear();
                 addDriverNumber.clear();
                 addCoDriverNumber.clear();
+                addTripNumber.clear();
+                addDepartureTime.clear();
+                addReturnTime.clear();
+                addMileage.clear();
+                addGasUsed.clear();
+                addGasPurchased.clear();
+                addGasPrice.clear();
+                addState.clear();
+                addModel.clear();
+                addColor.clear();
             }
         });
 
