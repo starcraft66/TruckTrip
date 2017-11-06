4f52cfdecbae5cde6c525bda998302a113519fa4
Marc-eric Pothier <marc.eric.pothier@gmail.com>
2017-11-06 11:40:29 -0500

    userDao fix

--- src/co/tdude/trucktrip/UserDao.java
+++ src/co/tdude/trucktrip/UserDao.java
@@ -1,6 +1,5 @@
 package co.tdude.trucktrip;
 
-import co.tdude.crud.model.User;
 import co.tdude.trucktrip.model.TruckTrip;
 
 import java.sql.*;
@@ -15,15 +14,26 @@ public class UserDao {
         connection = DbUtil.getConnection();
     }
 
-    public void addTrip(TruckTrip user) {
+    public void addTrip(TruckTrip trip) {
         try {
             PreparedStatement preparedStatement = connection
-                    .prepareStatement("insert into users(firstname,lastname,dob,email) values (?, ?, ?, ? )");
+                    .prepareStatement("insert into trips(tripNumber,truckNumber,driverNumber,coDriverNumber,departureTime,arrivalTime," +
+                                                            "mileage,gasUsed,gasPurchased,gasPrice,state,model,color) " +
+                                                            "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, )");
             // Parameters start with 1
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setDate(3, new java.sql.Date(user.getDob().getTime()));
-            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.setString(1,  Integer.toString(trip.getTripNumber()));
+            preparedStatement.setString(2, Integer.toString(trip.getTruckNumber()));
+            preparedStatement.setString(3, Integer.toString(trip.getDriverNumber()));
+            preparedStatement.setString(4, Integer.toString(trip.getCoDriverNumber()));
+            preparedStatement.setDate(5, (Date) trip.getDepartureTime());
+            preparedStatement.setDate(6, (Date) trip.getArrivalTime());
+            preparedStatement.setString(7,  Double.toString(trip.getMileage()));
+            preparedStatement.setString(8,  Double.toString(trip.getGasUsed()));
+            preparedStatement.setString(9,  Double.toString(trip.getGasPurchased()));
+            preparedStatement.setString(10,  Double.toString(trip.getGasPrice()));
+            preparedStatement.setString(11,  trip.getState());
+            preparedStatement.setString(12,  trip.getModel());
+            preparedStatement.setString(13,  trip.getColor());
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
@@ -34,9 +44,9 @@ public class UserDao {
     public void deleteTrip(int tripId) {
         try {
             PreparedStatement preparedStatement = connection
-                    .prepareStatement("delete from users where userid=?");
+                    .prepareStatement("delete from trips where tripNumber=?");
             // Parameters start with 1
-            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(1, tripId);
             preparedStatement.executeUpdate();
 
         } catch (SQLException e) {
@@ -47,61 +57,88 @@ public class UserDao {
     public void updateTrip(TruckTrip trip) {
         try {
             PreparedStatement preparedStatement = connection
-                    .prepareStatement("update users set firstname=?, lastname=?, dob=?, email=?" +
-                            "where userid=?");
+                    .prepareStatement("update trips set tripNumber=?,truckNumber=?,driverNumber=?,coDriverNumber=?,departureTime=?,arrivalTime=?," +
+                                            "mileage=?,gasUsed=?,gasPurchased=?,gasPrice=?,state=?,model=?,color=?) " +
+                                            "where tripNumber=?");
             // Parameters start with 1
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setDate(3, new java.sql.Date(user.getDob().getTime()));
-            preparedStatement.setString(4, user.getEmail());
-            preparedStatement.setInt(5, user.getUserid());
-            preparedStatement.executeUpdate();
+            preparedStatement.setString(1,  Integer.toString(trip.getTripNumber()));
+            preparedStatement.setString(2, Integer.toString(trip.getTruckNumber()));
+            preparedStatement.setString(3, Integer.toString(trip.getDriverNumber()));
+            preparedStatement.setString(4, Integer.toString(trip.getCoDriverNumber()));
+            preparedStatement.setDate(5, (Date) trip.getDepartureTime());
+            preparedStatement.setDate(6, (Date) trip.getArrivalTime());
+            preparedStatement.setString(7,  Double.toString(trip.getMileage()));
+            preparedStatement.setString(8,  Double.toString(trip.getGasUsed()));
+            preparedStatement.setString(9,  Double.toString(trip.getGasPurchased()));
+            preparedStatement.setString(10,  Double.toString(trip.getGasPrice()));
+            preparedStatement.setString(11,  trip.getState());
+            preparedStatement.setString(12,  trip.getModel());
+            preparedStatement.setString(13,  trip.getColor());
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public List<TruckTrip> getAllUsers() {
-        List<TruckTrip> users = new ArrayList<>();
+    public List<TruckTrip> getAllTrips() {
+        List<TruckTrip> trips = new ArrayList<>();
         try {
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from users");
+            ResultSet rs = statement.executeQuery("select * from trips");
             while (rs.next()) {
-                TruckTrip user = new TruckTrip();
-                user.setUserid(rs.getInt("userid"));
-                user.setFirstName(rs.getString("firstname"));
-                user.setLastName(rs.getString("lastname"));
-                user.setDob(rs.getDate("dob"));
-                user.setEmail(rs.getString("email"));
-                users.add(user);
+                TruckTrip trip = new TruckTrip();
+                trip.setTruckNumber(rs.getInt("truckNumber"));
+                trip.setDriverNumber(rs.getInt("driverNumber"));
+                trip.setCoDriverNumber(rs.getInt("coDriverNumber"));
+                trip.setTripNumber(rs.getInt("tripNumber"));
+                trip.setDepartureTime(rs.getDate("departureTime"));
+                trip.setArrivalTime(rs.getDate("arrivalTime"));
+                trip.setMileage(rs.getDouble("mileage"));
+                trip.setGasUsed(rs.getDouble("gasUsed"));
+                trip.setGasPurchased(rs.getDouble("gasPurchased"));
+                trip.setGasPrice(rs.getDouble("gasPrice"));
+                trip.setState(rs.getString("state"));
+                trip.setModel(rs.getString("model"));
+                trip.setColor(rs.getString("color"));
+                trips.add(trip);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return users;
+        return trips;
     }
 
     public TruckTrip getUserById(int userId) {
-        TruckTrip user = new TruckTrip();
+        TruckTrip trip = new TruckTrip();
         try {
             PreparedStatement preparedStatement = connection.
-                    prepareStatement("select * from users where userid=?");
+                    prepareStatement("select * from trips where tripNumber=?");
             preparedStatement.setInt(1, userId);
             ResultSet rs = preparedStatement.executeQuery();
 
             if (rs.next()) {
-                user.setUserid(rs.getInt("userid"));
-                user.setFirstName(rs.getString("firstname"));
-                user.setLastName(rs.getString("lastname"));
-                user.setDob(rs.getDate("dob"));
-                user.setEmail(rs.getString("email"));
+                trip.setTruckNumber(rs.getInt("truckNumber"));
+                trip.setDriverNumber(rs.getInt("driverNumber"));
+                trip.setCoDriverNumber(rs.getInt("coDriverNumber"));
+                trip.setTripNumber(rs.getInt("tripNumber"));
+                trip.setDepartureTime(rs.getDate("departureTime"));
+                trip.setArrivalTime(rs.getDate("arrivalTime"));
+                trip.setMileage(rs.getDouble("mileage"));
+                trip.setGasUsed(rs.getDouble("gasUsed"));
+                trip.setGasPurchased(rs.getDouble("gasPurchased"));
+                trip.setGasPrice(rs.getDouble("gasPrice"));
+                trip.setState(rs.getString("state"));
+                trip.setModel(rs.getString("model"));
+                trip.setColor(rs.getString("color"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return user;
+        return trip;
     }
+
+
+
 }
