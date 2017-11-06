678db1ec8c6d9b72f406163c41caa86d859143a5
Tristan Gosselin-Hane <starcraft66@gmail.com>
2017-11-06 11:09:49 -0500

    Initial SQL code

---
+++ src/co/tdude/trucktrip/UserDao.java
@@ -0,0 +1,107 @@
+package co.tdude.trucktrip;
+
+import co.tdude.crud.model.User;
+import co.tdude.trucktrip.model.TruckTrip;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+
+    private Connection connection;
+
+    public UserDao() {
+        connection = DbUtil.getConnection();
+    }
+
+    public void addTrip(TruckTrip user) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("insert into users(firstname,lastname,dob,email) values (?, ?, ?, ? )");
+            // Parameters start with 1
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setDate(3, new java.sql.Date(user.getDob().getTime()));
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTrip(int tripId) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("delete from users where userid=?");
+            // Parameters start with 1
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateTrip(TruckTrip trip) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("update users set firstname=?, lastname=?, dob=?, email=?" +
+                            "where userid=?");
+            // Parameters start with 1
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setDate(3, new java.sql.Date(user.getDob().getTime()));
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.setInt(5, user.getUserid());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<TruckTrip> getAllUsers() {
+        List<TruckTrip> users = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from users");
+            while (rs.next()) {
+                TruckTrip user = new TruckTrip();
+                user.setUserid(rs.getInt("userid"));
+                user.setFirstName(rs.getString("firstname"));
+                user.setLastName(rs.getString("lastname"));
+                user.setDob(rs.getDate("dob"));
+                user.setEmail(rs.getString("email"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public TruckTrip getUserById(int userId) {
+        TruckTrip user = new TruckTrip();
+        try {
+            PreparedStatement preparedStatement = connection.
+                    prepareStatement("select * from users where userid=?");
+            preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                user.setUserid(rs.getInt("userid"));
+                user.setFirstName(rs.getString("firstname"));
+                user.setLastName(rs.getString("lastname"));
+                user.setDob(rs.getDate("dob"));
+                user.setEmail(rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+}
